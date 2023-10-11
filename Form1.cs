d185be1b5f06353d5ea5a94b4c4f898614affbf8
Limuncic <145533801+Limuncic@users.noreply.github.com>
2023-10-11 09:31:03 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10._4.Rafael
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // upis trenutnog vremena u label
+            label2.Text =DateTime.Now.ToShortTimeString();
+
+            // isprobati više različitih ispisa vremena
+
+        }
+    }
+}
